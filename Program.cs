dec53b805cbd526164e0c8992385959934d5c859
Richard Macaskill <richard.macaskill@neotechnology.com>
2023-05-02 10:01:41 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Neo4j.Driver;
+
+var uri = "neo4j+s://cas-testlab-neo4j.co.uk:7687";
+var username = "moviesuser";
+var password = Environment.GetEnvironmentVariable("NEO4J_PWD");
+
+var driver = GraphDatabase.Driver(uri, AuthTokens.Basic(username, password));
+
+var records = await driver.ExecutableQuery("MATCH(m:Movie) return m.title as moviename LIMIT 1;").ExecuteAsync();
+
+Console.WriteLine(records.Result[0].Values["moviename"]);
